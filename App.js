796aac3b4be08fad23a1287d461e32036acd56c7
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-19 22:47:22 +0200

    Add QR code scanning and improve design

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 /* 1st party libraries and dependencies: react, react native and Expo stuff */
 
 import { StatusBar } from 'expo-status-bar';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
 import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
@@ -16,165 +18,26 @@ import {
 
 /* 3rd party libraries */
 
-import { iclipUri } from './Vars.js'
+import { iclipUrif } from './Vars.js';
 import { Header, Input, Icon } from 'react-native-elements';
 
-/* Function and config */
-const checkError = (msg) => {
-  return msg.indexOf('Error: ') > -1;
-};
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const ValidationMsg = (txt) => {
-  txt = txt.replace(' ', '').toLowerCase();
-  const diff = config.codeMaxLength - txt.length;
-  if (txt.match(config.charRegex)) {
-    return `There are some characters, that shouldn't be there.`;
-  } else {
-    if ((txt.length < config.codeMaxLength) & (txt.length > 0)) {
-      return `${diff} more character${diff === 1 ? '' : 's'} please`;
-    } else if (txt.length === 0) {
-      return `Just type in the code above and see the magic happen.`;
-    }
-  }
-};
-
-const entireScreenHeight = Dimensions.get('window').height;
-
-const config = {
-  codeMaxLength: 5, // The code's length has to be always 5 characters
-  charRegex: new RegExp('[^A-Za-z0-9]'), // Only allow ascii characters to be entered as the code
-};
-/* Styles */
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center', // Centered horizontally
-    justifyContent: 'center',
-    flex: 1,
-  },
-  tinyLogo: {
-    width: 30,
-    height: 30,
-  },
-});
-/* Colors and stuff */
-const colors = {
-  bg: 'white',
-  headerBg: '#333333',
-  text: 'black',
-  errorColor: '#f44336',
-  light: 'white',
-};
+/* Pages */
+import { HomeScreen, QRScreen } from './Pages.js';
 
 /* App component */
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  /* Variable set */
-  const [isLoading, setLoading] = useState(true); // Loading status => only show the responce of the API after the request completes
-  const [data, setData] = useState(''); // Dynamically loaded data from the Interclip REST API
-  const [text, setText] = useState(''); // The code entered in the <Input>
-  const [progress, setProgress] = useState('');
-
-  useEffect(() => {
-    if (text.length === config.codeMaxLength) {
-      setText(text.replace(' ', '').toLowerCase());
-      fetch(`http://uni.hys.cz/includes/get-api?user=${text}`)
-        .then((response) => response.text())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    } else {
-      setLoading(true);
-    }
-  }, [text]);
-
   return (
-    <View>
-      <Header
-        containerStyle={{
-          backgroundColor: colors.headerBg,
-          justifyContent: 'space-around',
-          marginBottom: Platform.OS === 'ios' ? '50%' : '25%',
-        }}>
-        <Icon
-          onPress={() => {
-            alert('QR code more like bruh-r code');
-          }}
-          type="font-awesome" // The icon is loaded from the font awesome icon library
-          name="qrcode" // Icon fa-qrcode
-          color="#fff" // White color for contrast on the Header
-        />
-
-        <Text style={{ color: 'white', fontSize: 30 }}>Interclip</Text>
-
-        <Image
-          style={styles.tinyLogo}
-          source={{
-            uri: iclipUri,
-          }}
-        />
-      </Header>
-
-      <View>
-        <Input
-          keyboardType={
-            Platform.OS === 'android' ? 'email-address' : 'ascii-capable'
-          }
-          style={styles.container}
-          placeholder="Your code here"
-          maxLength={config.codeMaxLength}
-          inputStyle={{ fontSize: 50 }}
-          autoCorrect={false}
-          returnKeyType={'go'}
-          onChangeText={(text) => setText(text)}
-          defaultValue={text}
-          errorStyle={{ color: 'red' }}
-          autoCapitalize="none"
-          autoFocus={true}
-          value={text.replace(' ', '').toLowerCase()}
-          enablesReturnKeyAutomatically={true}
-          onSubmitEditing={() => {
-            !isLoading
-              ? Linking.openURL(data)
-              : alert(
-                  `No URL set yet, make sure your code is ${config.codeMaxLength} characters long!`
-                );
-          }}
-        />
-
-        <View style={{ padding: 24 }}>
-          <Text
-          >{ValidationMsg(text)}</Text>
-        </View>
-        <View style={{ padding: 24 }}>
-          {isLoading ? (
-            <Text></Text>
-          ) : (
-            <Text
-              onLongPress={() => {
-                /* Handle functionality, when user presses for a longer period of time */
-                Clipboard.setString(data);
-                alert('Copied to Clipboard!');
-              }}
-              onPress={() => {
-                Linking.openURL(data)
-              }}
-              style={{
-                color: checkError(data) ? colors.light : colors.text,
-                backgroundColor:
-                  checkError(data) & !ValidationMsg(text)
-                    ? colors.errorColor
-                    : null,
-                fontSize: 20,
-              }}>
-              {!ValidationMsg(text) &&
-                (checkError(data)
-                  ? "This code doesn't seem to exist 🤔"
-                  : data)}
-            </Text>
-          )}
-        </View>
-        <StatusBar style="light" />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="QR" component={QRScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ Pages.js
@@ -0,0 +1,255 @@
+import { StatusBar } from 'expo-status-bar';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { NavigationContainer } from '@react-navigation/native';
+
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Linking,
+  Clipboard,
+  Image,
+  Dimensions,
+  Platform,
+  Button,
+  Alert,
+  DynamicColorIOS,
+  Vibration,
+} from 'react-native';
+
+/* 3rd party libraries */
+
+import { iclipUri } from './Vars.js';
+import { Header, Input, Icon } from 'react-native-elements';
+
+/* Function and config */
+const checkError = (msg) => {
+  return msg.indexOf('Error: ') > -1;
+};
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+const ValidationMsg = (txt) => {
+  txt = txt.replace(' ', '').toLowerCase();
+  const diff = config.codeMaxLength - txt.length;
+  if (txt.match(config.charRegex)) {
+    return `There are some characters, that shouldn't be there.`;
+  } else {
+    if ((txt.length < config.codeMaxLength) & (txt.length > 0)) {
+      return `${diff} more character${diff === 1 ? '' : 's'} please`;
+    } else if (txt.length === 0) {
+      return `Just type in the code above and see the magic happen.`;
+    }
+  }
+};
+
+const entireScreenHeight = Dimensions.get('window').height;
+
+const config = {
+  codeMaxLength: 5, // The code's length has to be always 5 characters
+  charRegex: new RegExp('[^A-Za-z0-9]'), // Only allow ascii characters to be entered as the code
+};
+/* Styles */
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center', // Centered horizontally
+    justifyContent: 'center',
+    flex: 1,
+  },
+  tinyLogo: {
+    width: 30,
+    height: 30,
+  },
+});
+/* Colors and stuff */
+const colors = {
+  bg: 'white',
+  headerBg: '#333333',
+  text: 'black',
+  errorColor: '#f44336',
+  light: 'white',
+};
+/*
+if(Platform.OS === "ios") {
+const customDynamicTextColor = DynamicColorIOS({
+  dark: '#fff',
+  light: colors.text
+});
+
+const dynamicBgColor = DynamicColorIOS({
+  dark: '#444444',
+  light: colors.bg
+});
+}
+*/
+export function HomeScreen({ navigation }) {
+  /* Variable set */
+  const [isLoading, setLoading] = useState(true); // Loading status => only show the responce of the API after the request completes
+  const [data, setData] = useState(''); // Dynamically loaded data from the Interclip REST API
+  const [text, setText] = useState(''); // The code entered in the <Input>
+  const [progress, setProgress] = useState('');
+
+  useEffect(() => {
+    if (text.length === config.codeMaxLength) {
+      setText(text.replace(' ', '').toLowerCase());
+      fetch(`http://uni.hys.cz/includes/get-api?user=${text}`)
+        .then((response) => response.text())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(true);
+    }
+  }, [text]);
+
+  return (
+    <View style={{ backgroundColor: '' }}>
+      <Header
+        containerStyle={{
+          //backgroundColor: colors.headerBg,
+          backgroundColor: 'white',
+          justifyContent: 'space-around',
+          marginBottom: Platform.OS === 'ios' ? '45%' : '25%',
+        }}>
+        <Icon
+          onPress={() => navigation.navigate('QR')}
+          type="font-awesome" // The icon is loaded from the font awesome icon library
+          name="qrcode" // Icon fa-qrcode
+          color="#000" // White color for contrast on the Header
+        />
+
+        <Text style={{ fontSize: 30 }}>Interclip</Text>
+
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: iclipUri,
+          }}
+        />
+      </Header>
+
+      <View>
+        <Input
+          keyboardType={
+            Platform.OS === 'android' ? 'email-address' : 'ascii-capable'
+          }
+          style={styles.container}
+          placeholder="Your code here"
+          maxLength={config.codeMaxLength}
+          inputStyle={{ fontSize: 50 }}
+          autoCorrect={false}
+          returnKeyType={'go'}
+          onChangeText={(text) => setText(text)}
+          defaultValue={text}
+          errorStyle={{ color: 'red' }}
+          autoCapitalize="none"
+          autoFocus={true}
+          value={text.replace(' ', '').toLowerCase()}
+          enablesReturnKeyAutomatically={true}
+          onSubmitEditing={() => {
+            !isLoading
+              ? Linking.openURL(data)
+              : alert(
+                  `No URL set yet, make sure your code is ${config.codeMaxLength} characters long!`
+                );
+          }}
+        />
+        {ValidationMsg(text) && (
+          <View style={{ padding: 24 }}>
+            <Text>{ValidationMsg(text)}</Text>
+          </View>
+        )}
+        <View style={{ padding: 24 }}>
+          {isLoading ? (
+            <Text></Text>
+          ) : (
+            <Text
+              onLongPress={() => {
+                /* Handle functionality, when user presses for a longer period of time */
+                Clipboard.setString(data);
+                alert('Copied to Clipboard!');
+              }}
+              onPress={() => {
+                Linking.openURL(data);
+              }}
+              style={{
+                color: checkError(data) ? colors.light : colors.text,
+                backgroundColor:
+                  checkError(data) & !ValidationMsg(text)
+                    ? colors.errorColor
+                    : null,
+                fontSize: 20,
+              }}>
+              {!ValidationMsg(text) &&
+                (checkError(data)
+                  ? "This code doesn't seem to exist 🤔"
+                  : data)}
+            </Text>
+          )}
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+export function QRScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    if (
+      (data.indexOf('https://iclip.netlify.com') > -1) |
+      (data.indexOf('http://iclip.netlify.app') > -1)
+    ) {
+      Vibration.vibrate();
+      navigation.navigate('Home');
+      Linking.openURL(data);
+      setScanned(false);
+    } else {
+          Alert.alert("This doesn't appear to be an Interclip URL","Do you still want to open it?",
+       [
+    {
+      text: 'Cancel',
+      onPress: () => {setScanned(true); sleep(1000).then(setScanned(false))},
+      style: 'cancel'
+    },
+    { text: 'Sure', onPress: () =>  {Linking.openURL(data); setScanned(false);}
+ }
+      ])
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+      }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+    </View>
+  );
+}
