2f5d2ddfe2497961aa84c0d225cfb65b73c415c9
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-20 12:19:36 +0200

    Better navigation

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 /* 1st party libraries and dependencies: react, react native and Expo stuff */
 
 import { StatusBar } from 'expo-status-bar';
-import { BarCodeScanner } from 'expo-barcode-scanner';
 
 import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator,
-  Linking,
-  Clipboard,
-  Image,
-  Dimensions,
-  Platform,
-} from 'react-native';
 
 /* 3rd party libraries */
 
-import { iclipUrif } from './Vars.js';
 import { Header, Input, Icon } from 'react-native-elements';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -30,6 +17,15 @@ import { HomeScreen, QRScreen } from './Pages.js';
 /* App component */
 
 const Stack = createStackNavigator();
+
+const root = () => {
+  return(
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="QR" component={QRScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
