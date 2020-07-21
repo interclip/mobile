9e1635cf63090bfb52c4616a13f99570a2639acf
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-21 09:20:11 +0200

    Update header to not show on homepage

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
         <Stack.Screen name="QR" component={QRScreen} />
         <Stack.Screen name="Settings" component={SettingsPage} />
       </Stack.Navigator>
