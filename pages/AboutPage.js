879fb0c292be439f981088989d775f9d1b9b5cf5
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-28 20:47:24 +0200

    refactor: Reformat AboutPage.js

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,19 +1,14 @@
-// React, React Native imports 
+// React, React Native imports
 
-import React, { useState } from 'react';
-import {
-  Text,
-  useColorScheme,
-  View,
-  Dimensions,
-} from 'react-native';
+import React, { useState } from "react";
+import { Text, useColorScheme, View, Dimensions } from "react-native";
 
-import * as Linking from 'expo-linking';
+import * as Linking from "expo-linking";
 
-// Local functions, components and variables 
+// Local functions, components and variables
 
-import { colors } from '../lib/Pages';
-import * as appInfo from '../app.json';
+import { colors } from "../lib/Pages";
+import * as appInfo from "../app.json";
 
 import { Icon } from "react-native-elements";
 
@@ -31,54 +26,61 @@ export function AboutPage() {
         padding: 25,
         flex: 1,
         backgroundColor:
-          colorScheme === 'dark' ? colors.darkContent : colors.lightContent,
-        paddingTop: "10%"
+          colorScheme === "dark" ? colors.darkContent : colors.lightContent,
+        paddingTop: "10%",
       }}
     >
       <LogoImage />
       <Text
         style={{
-          color: colorScheme === 'dark' ? 'white' : 'black',
+          color: colorScheme === "dark" ? "white" : "black",
           fontSize: 20,
-          textAlign: 'center'
+          textAlign: "center",
         }}
       >
-        Interclip mobile is the mobile companion to Interclip, an awesome tool for sharing URLs and files cross-device and cross-platform
+        Interclip mobile is the mobile companion to Interclip, an awesome tool
+        for sharing URLs and files cross-device and cross-platform
       </Text>
       <View
-            style={{
-                display: "flex",
-                marginTop: "10%",
-                flexDirection: "row",
-                justifyContent: "space-around"
-            }}
-        >
-            <Icon
-                onPress={() => Linking.openURL("https://github.com/filiptronicek/iclip-mobile/")}
-                type="font-awesome" // The icon is loaded from the font awesome icon library
-                name="github" // Icon fa-github
-                color={colorScheme === "dark" ? "white" : "black"} // White color for contrast on the Header
-                size={50}
-            />
-            <Icon
-                onPress={() => Linking.openURL("https://twitter.com/filiptronicek")}
-                type="font-awesome" // The icon is loaded from the font awesome icon library
-                name="twitter" // Icon fa-twitter
-                color={colorScheme === "dark" ? "white" : "black"} // White color for contrast on the Header
-                size={50}
-            />
-        </View>
+        style={{
+          display: "flex",
+          marginTop: "10%",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <Icon
+          onPress={() =>
+            Linking.openURL("https://github.com/filiptronicek/iclip-mobile/")
+          }
+          type="font-awesome" // The icon is loaded from the font awesome icon library
+          name="github" // Icon fa-github
+          color={colorScheme === "dark" ? "white" : "black"} // White color for contrast on the Header
+          size={50}
+        />
+        <Icon
+          onPress={() => Linking.openURL("https://twitter.com/filiptronicek")}
+          type="font-awesome" // The icon is loaded from the font awesome icon library
+          name="twitter" // Icon fa-twitter
+          color={colorScheme === "dark" ? "white" : "black"} // White color for contrast on the Header
+          size={50}
+        />
+      </View>
       <Text
         style={{
-          position: 'absolute',
-          bottom: '5%',
-          left: Dimensions.get('window').width / 2 - versionWidth / 2,
-          color: colorScheme === 'dark' ? colors.secondary : 'grey',
+          position: "absolute",
+          bottom: "5%",
+          left: Dimensions.get("window").width / 2 - versionWidth / 2,
+          color: colorScheme === "dark" ? colors.secondary : "grey",
         }}
         onLayout={(event) => setVersionWidth(event.nativeEvent.layout.width)}
-        onPress={() => Linking.openURL(`https://github.com/filiptronicek/iclip-mobile/releases/tag/v${appInfo.expo.version}`)}
+        onPress={() =>
+          Linking.openURL(
+            `https://github.com/filiptronicek/iclip-mobile/releases/tag/v${appInfo.expo.version}`
+          )
+        }
       >
-        Interclip mobile v{appInfo.expo.version}{' '}
+        Interclip mobile v{appInfo.expo.version}{" "}
       </Text>
     </View>
   );
