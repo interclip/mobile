f1fc6ee79359890f47b97cf35a0d163094bff27e
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-30 17:28:39 +0200

    test: add some tests

---
+++ tests/numbers.test.js
@@ -0,0 +1,10 @@
+import { formatBytes } from "../lib/functions";
+import { test, expect } from '@jest/globals';
+
+test('checks if Interclip\'s URL is a valid one', () => {
+    expect(formatBytes(25)).toBe("25 Bytes");
+});
+
+test('checks if a malformed URL is a valid one', () => {
+    expect(formatBytes(45000000000)).toBe("41.91 GB");
+});
